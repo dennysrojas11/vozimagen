02894c7595941bdd0559774b8ed6f1099014c430
Dennys Rojas <65422210+dennysrojas11@users.noreply.github.com>
2020-11-08 13:38:47 -0500

    Carga del proyecto
    Carga del proyecto que transforma de voz a imágenes

---
+++ WinAppVozImagen/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace WinAppVozImagen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.labelTitulo = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonVoz = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelTitulo
+            // 
+            this.labelTitulo.AutoSize = true;
+            this.labelTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitulo.Location = new System.Drawing.Point(185, 40);
+            this.labelTitulo.Name = "labelTitulo";
+            this.labelTitulo.Size = new System.Drawing.Size(325, 39);
+            this.labelTitulo.TabIndex = 0;
+            this.labelTitulo.Text = "De voz a imágenes";
+            this.labelTitulo.Click += new System.EventHandler(this.labelTitulo_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(39, 186);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(318, 30);
+            this.textBox1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(407, 104);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(368, 289);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // buttonVoz
+            // 
+            this.buttonVoz.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonVoz.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.buttonVoz.FlatAppearance.BorderSize = 0;
+            this.buttonVoz.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.buttonVoz.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.buttonVoz.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonVoz.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonVoz.Image = ((System.Drawing.Image)(resources.GetObject("buttonVoz.Image")));
+            this.buttonVoz.Location = new System.Drawing.Point(39, 272);
+            this.buttonVoz.Name = "buttonVoz";
+            this.buttonVoz.Size = new System.Drawing.Size(128, 101);
+            this.buttonVoz.TabIndex = 3;
+            this.buttonVoz.UseVisualStyleBackColor = true;
+            this.buttonVoz.Click += new System.EventHandler(this.buttonVoz_Click);
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(229, 272);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 101);
+            this.button1.TabIndex = 4;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonVoz);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelTitulo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTitulo;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonVoz;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WinAppVozImagen/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+
+namespace WinAppVozImagen
+{
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine objVoz = new SpeechRecognitionEngine();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void labelTitulo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            buttonVoz.Focus();
+            button1.Enabled = false;
+            pictureBox1.Visible = false;
+        }
+
+        private void buttonVoz_Click(object sender, EventArgs e)
+        {
+            objVoz.SetInputToDefaultAudioDevice();
+            objVoz.LoadGrammar(new DictationGrammar());
+            objVoz.SpeechRecognized += enlazar;
+            objVoz.RecognizeAsync(RecognizeMode.Multiple);
+            buttonVoz.Enabled = false;
+            button1.Enabled = true;
+        }
+
+        void enlazar(object sender, SpeechRecognizedEventArgs e)
+        {
+            textBox1.Text = e.Result.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            buttonVoz.Enabled = true;
+            button1.Enabled = false;
+            objVoz.RecognizeAsyncStop();
+            pictureBox1.Visible = true;
+            if (textBox1.Text == "Auto" || textBox1.Text == "Carro" || textBox1.Text == "Automóvil")
+            {
+                pictureBox1.Image = Properties.Resources.auto;
+                pictureBox1.Refresh();
+            }
+            else if (textBox1.Text == "Pelota" || textBox1.Text == "Balón")
+            {
+                pictureBox1.Image = Properties.Resources.pelota;
+                pictureBox1.Refresh();
+            }
+            else if (textBox1.Text == "Casa")
+            {
+                pictureBox1.Image = Properties.Resources.casa;
+                pictureBox1.Refresh();
+            }
+            else {
+                pictureBox1.Image = Properties.Resources._404;
+                pictureBox1.Refresh();
+            }
+        }
+    }
+}
